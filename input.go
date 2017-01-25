8593312fd06ed697c420cd2826ab91aa5511f765
Jonas Xavier <jonas.agx@gmail.com>
2017-01-25 14:28:05 -0800

    New version with utils

--- input.go
+++ input.go
@@ -1,24 +1,8 @@
 package main
 
 // Libraries used for this example
-import (
-   "bufio"	//<-- implements buffered I/O.
-   "fmt"	//<-- implements formatted I/O with functions analogous to C's printf and scanf.
-   "os"		//<-- provides a platform-independent interface to operating system functionality.
-)
+import "github.com/jonasagx/csutils"
 
 func main() {
-	//Prints the message
-	fmt.Print("Write your name then press ENTER, please: ")
-	
-	//Reads the text input
-	var scan = bufio.NewScanner(os.Stdin)
-	
-	//Check if anything was passed as input and extract it
-	scan.Scan()
-	//Put the input in a variable of type string
-	var name string = scan.Text()
-	
-	//Prints the input on the terminal
-	fmt.Println("Hi " + name)
+	csutils.Print("Write your name then press ENTER, please: ")
 }
