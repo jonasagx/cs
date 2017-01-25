d0e4abceacc9120a503ae1c1a191858995492e4b
Jonas Xavier <jonas.agx@gmail.com>
2017-01-25 14:36:07 -0800

    Remove package name

--- input.go
+++ input.go
@@ -1,7 +1,7 @@
 package main
 
-// Libraries used for this example
-import "github.com/jonasagx/csutils"
+// Library used for this example
+import . "github.com/jonasagx/csutils"
 
 func main() {
 	csutils.Print("Write your name then press ENTER, please: ")
