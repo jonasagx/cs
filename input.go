433a1fdad803115909058f7d722ef67b5e4e544f
Jonas Xavier <jonas.agx@gmail.com>
2017-01-25 16:47:12 -0800

    Improving input example

--- input.go
+++ input.go
@@ -4,6 +4,9 @@ package main
 import . "github.com/jonasagx/csutils"
 
 func main() {
-	var name string = ReadInput("Write your name then press ENTER, please: ")
-	Print(name)
+	var name string = ReadName("Write your name then press ENTER, please: ")
+	Print("The name", name, "has", len(name), "characters")
+
+	var ageInDays int = ReadInteger("Write your age please, then press ENTER: ")
+	Print("You've lived", ageInDays, "days")
 }
