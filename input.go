bc3fadf241d221c9fafec60ef389cd98bd946ea8
Jonas Xavier <jonas.agx@gmail.com>
2017-01-25 17:15:00 -0800

    Clear new version

--- input.go
+++ input.go
@@ -4,9 +4,12 @@ package main
 import . "github.com/jonasagx/csutils"
 
 func main() {
+	const daysPerYear int = 365
+
 	var name string = ReadName("Write your name then press ENTER, please: ")
 	Print("The name", name, "has", len(name), "characters")
 
-	var ageInDays int = ReadInteger("Write your age please, then press ENTER: ")
-	Print("You've lived", ageInDays, "days")
+	var ageInYears int = ReadInteger("Write your age please, then press ENTER: ")
+	var ageInDays = ageInYears * daysPerYear
+	Print(name, "you've lived", ageInDays, "days")
 }
